8253617425c705f31c580ba57bb5dc1d4877d477
sauraabbhh2004 <saurabhyaul.1234@gmail.com>
2024-10-29 20:30:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GymApp
+ React gym workout generator
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from './Button'
+
+export default function Hero() {
+    return (
+        <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[1000px] w-full mx-auto p-4'>
+            <div className='fiex flex-col gap-4'>
+                <p>
+                    IT'S TIME TO GET
+                </p>
+                <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>
+                    Swole<span className='text-blue-400'>Gym</span>
+                </h1>
+            </div>
+            <p className='text-sm md:text-base font-light'>
+                I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>unbelievable swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.
+            </p>
+            <Button func={() => {
+                window.location.href = '#generate'
+            }} text={"Accept & Begin"}></Button>
+        </div>
+    )
+}
